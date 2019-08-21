175e0796e73ba593d83ad164a0a6d3ee05d165e9
Sergio <sera.serduk@gmail.com>
2019-08-21 18:29:43 +0300

    adding styles

---
+++ src/scenes/homeScene/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const module_style = StyleSheet.create({
+  timeline_wrapper: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+});
+
+export default module_style;
---
+++ src/themes/default_styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const module_style = StyleSheet.create({
+  container_wrapper: {
+    paddingHorizontal: 30,
+    paddingVertical: 30,
+  },
+});
+
+export default module_style;
